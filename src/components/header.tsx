1a7931002896481b38f43a06bc09eb5edf989011
Kaz Tsuji <y9c.elm4a@gmail.com>
2025-09-15 14:20:50 +0900

    try it

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface HeaderProps {
   smoothScroll: (id: string) => void;
@@ -7,11 +8,20 @@ const Header = ({ smoothScroll }: HeaderProps) => (
   //<header className="fixed top-0 left-0 w-full bg-white shadow-md z-50 transition-shadow duration-300">
   <header className="fixed top-0 left-0 w-full bg-red-500 shadow-md z-50">
 
-    <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+    {/*<div className="container mx-auto px-6 py-4 flex justify-between items-center">*/}
       {/* 会社ロゴ */}
       <div className="flex items-center space-x-2">
+        <Image
+          src="/comLogo.jpg"
+          alt="Company Logo"
+          width={288}   // w-72 と同じ
+          height={80}   // 適当な高さ
+          className="h-auto"
+        />
+      {/*</div>
+      <div className="flex items-center space-x-2">
         <img src="/comLogo.jpg" alt="Company Logo" className="w-72 h-auto" />
-      </div>
+      </div>*/}
 
       {/* ナビゲーションメニュー */}
       <nav className="hidden md:flex space-x-12 text-xl font-medium">

d7c782f1e0c79d0404a1285278321df905260f3e
Kaz Tsuji <y9c.elm4a@gmail.com>
2025-09-17 15:20:21 +0900

    rewrite sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,6 @@
+// next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://tsujikawa-tosou.vercel.app',
-  generateRobotsTxt: true, // robots.txtの生成を有効にする
-  sitemapSize: 7000,
-  outDir: './out', // Next.jsの`out`ディレクトリに出力する場合
-  // 他のオプションもここに追加できます
+  siteUrl: 'https://tsujikawa-tosou.vercel.app',
+  generateRobotsTxt: true,
 };
